5b67456042c1b34501cd572f63974102172971c3
Jawad Haider <it.xjawad0019@gmail.com>
2026-04-03 16:12:16 +0500

    added build

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,12 @@ import HeroSection from '../components/HeroSection';
 import aboutImage from '../assets/about.jpg';
 import Mission from '../components/Mission';
 import Believe from '../components/Believe';
-// import Team from '../components/Team';
+
 
 const About = () => {
     return (
         <div className="pt-20">
-            {/* Cinematic Hero Section */}
+
             <HeroSection
                 smallHeading="Our story"
                 mainHeading1="Built because getting"
